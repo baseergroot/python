aab8a7e840383ee9b0b03dcea080f2726408371e
Baseer Afridi <githubbs7@gmail.com>
2025-09-12 12:56:20 -0400

    tries added, user range selection added

--- number_quess/app.py
+++ number_quess/app.py
@@ -1,37 +1,47 @@
-import random
-import math
+from random  import random, randint
+from math import floor
 
 
-try:
-    user_number = int(input("Quess number between 1 - 100: "))
+lower_range = int(input("Enter the lower range: "))
+higher_range = int(input("Enter the higher range: "))
 
-    comp_number = math.floor(random.random() * 100)
-    print("computer select " , comp_number)
+if lower_range > higher_range:
+    print("Incorrect range: lower_range can not be greater that higher_range")
+else:
+    comp_number = randint(lower_range, higher_range)
 
-    if comp_number < user_number:
-        
-        if user_number - comp_number > 0 and user_number - comp_number <= 5:
-            print("Too close")
-        else:
-            print("Too High")
+    for i in range(3):
+        try:
+            user_number = int(input("Quess number between 1 - 100: "))
+
+            if comp_number < user_number:
+                
+                if user_number - comp_number > 0 and user_number - comp_number <= 6:
+                    print("close")
+                elif user_number - comp_number > 0 and user_number - comp_number <= 3:
+                    print("Too close")
+                else:
+                    print("Too High")
+
+            elif comp_number > user_number:
+
+                if comp_number - user_number  > 0 and comp_number - user_number <= 6:
+                    print("Close")
+                elif comp_number - user_number  > 0 and comp_number - user_number <= 3:
+                    print("Too close")
+                else:
+                    print("Too Low")
+
+            elif comp_number == user_number:
+                print("You win")
+
+            else:
+                print("Error: Invalid choice")
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    print("computer selection was " , comp_number)
 
 
-    elif comp_number > user_number:
-
-        if comp_number - user_number  > 0 and comp_number - user_number <= 5:
-            print("Too close")
-        else:
-            print("Too Low")
-
-    elif comp_number == user_number:
-        print("You win")
-
-    else:
-        print("Error: Invalid choice")
-
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
-
-
-
-# today i explored modules function, try and except, variables, type conversion, usage of math and random module, if else statement, and nested if else statement by building simple number quessing game project in python.
+# today i explored modules function, try and except, variables, type conversion, usage of math and random module, if else statement, and nested if else statement, for in loop, by building simple number quessing game project in python.
